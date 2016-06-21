0d9df6f50a93f6328ae68a7a73ca7fe0cc38d9e6
Jason Sadler <jsadler@netsuite.com>
2016-06-21 13:17:07 -0400

    Load from realm and show something in the collection view
    Doesn't load images just yet - just shows the list of image titles
    in a grid layout. Very basic mockup of what's to come

---
+++ ImageCollection/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  ImageCollection
+//
+//  Created by Jason Sadler on 2016-06-21.
+//  Copyright © 2016 Scattered Cloud Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCell : UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
--- ImageCollection/ImageCollectionViewController.swift
+++ ImageCollection/ImageCollectionViewController.swift
@@ -7,23 +7,60 @@
 //
 
 import UIKit
-import Alamofire
 import RealmSwift
 
-class ImageCollectionViewController: UICollectionViewController {
+class ImageCollectionViewController : UICollectionViewController {
 
     let photoManager = PhotoManager()
+    var realm : Realm!
+    var photos : Results<FlickrPhoto>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         photoManager.refreshPhotosFromFlickr()
+        
+        do {
+            realm = try Realm()
+            photos = realm.objects(FlickrPhoto)
+        } catch let e {
+            // TOOD
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ImageCollectionViewController /*: UICollectionViewDataSource*/ {
+    func photoAtIndexPath(indexPath: NSIndexPath) -> FlickrPhoto? {
+        let item = indexPath.indexAtPosition(1)
+        if item >= 0 && item < photos.count {
+            return photos[item]
+        }
+        return nil
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageCell", forIndexPath: indexPath) as! ImageCell
+        if let photo = photoAtIndexPath(indexPath) {
+            cell.label.text = photo.title
+        } else {
+            cell.label.text = "Ruh-roh"
+        }
+        return cell
+    }
+}
+
+extension ImageCollectionViewController : UICollectionViewDelegateFlowLayout /*, UICollectionViewDelegate*/ {
+    
+}
