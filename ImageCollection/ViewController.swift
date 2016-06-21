a5e95e9c06781ad6fd2125dc5667db13a772acf8
Jason Sadler <jsadler@netsuite.com>
2016-06-20 20:41:02 -0400

    Initial Commit

---
+++ ImageCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageCollection
+//
+//  Created by Jason Sadler on 2016-06-20.
+//  Copyright © 2016 Scattered Cloud Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
